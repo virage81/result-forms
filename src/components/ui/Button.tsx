a2673f4bdb02e1a81fed2b7f3fdce49350d8601a
virage81 <karimovminds@gmail.com>
2025-05-23 20:44:59 +0400

    chore: fix button styles

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,7 +7,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement>, PropsWithChildr
 }
 
 export const Button: FC<Props> = ({ className, to, children }) => {
-	const styles = `flex place-items-center border-2 border-purple-500 px-5 py-2 rounded-md transition-all hover:bg-purple-900 ${className}`;
+	const styles = `flex items-center justify-center border-2 border-purple-500 px-5 py-2 rounded-md transition-all hover:bg-purple-900 ${className}`;
 
 	if (typeof to !== 'undefined')
 		return (
